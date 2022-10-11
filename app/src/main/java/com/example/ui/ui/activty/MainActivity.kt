04b5f7ff8245191e3cb293d579b697bac169221f
Emirhan Siner <emirhansiner@gmail.com>
2022-10-11 12:33:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ui/ui/activty/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ui.ui.activty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
+import com.example.patikaweek3.R
+import com.example.ui.ui.fragment.DetailFragment
+import com.example.ui.ui.fragment.ListFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ app/src/main/java/com/example/ui/ui/fragment/DetailFragment.kt
@@ -0,0 +1,54 @@
+package com.example.ui.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.patikaweek3.R
+import com.example.ui.mockdata.CoinData
+
+
+class DetailFragment : Fragment() {
+    //Fragmenttan önce gelen verileri ayağa kaldırıyoruz
+    private lateinit var twCoinName: TextView
+    private lateinit var twCoinprice: TextView
+    private lateinit var twImageView: ImageView
+    //argüments ile dataları aldık
+    private val args by navArgs<DetailFragmentArgs>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews(view)
+        setCoinData()
+    }
+
+    private fun setCoinData() {
+        //gelen dataları coinData üzerinden alıp değişkenlere atadım
+        val coinData=args.coinData
+        twCoinName.text=coinData.coinName
+        twCoinprice.text=coinData.coinPrice
+        twImageView.setImageResource(coinData.image)
+    }
+
+    private fun setupViews(view: View) {
+        //Listview den gelen verileri xml' de atadık
+        twCoinName = view.findViewById(R.id.twDetailCoinname)
+        twCoinprice = view.findViewById(R.id.twDetailPrice)
+        twImageView = view.findViewById(R.id.ivDetail)
+
+
+    }
+}
